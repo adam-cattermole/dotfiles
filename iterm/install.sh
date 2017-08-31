187c6473eb677fabc686ec15f761772051ca2dce
Adam Cattermole <a.d.cattermole@gmail.com>
2017-08-31 17:30:20 +0100

    Fix to iterm font installation

--- iterm/install.sh
+++ iterm/install.sh
@@ -8,8 +8,8 @@ open "iterm/themes/DotGov.itermcolors"
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-FONT_NAME="Roboto Mono for Powerline.ttf"
-
 # Install font
-wget "https://github.com/powerline/fonts/blob/master/RobotoMono/Roboto%20Mono%20for%20Powerline.ttf?raw=true" -O "$FONT_NAME"
-cp iterm/"$FONT_NAME" ~/Library/Fonts/
+FONT_PATH="iterm/Roboto Mono for Powerline.ttf"
+wget "https://github.com/powerline/fonts/blob/master/RobotoMono/Roboto%20Mono%20for%20Powerline.ttf?raw=true" -O "$FONT_PATH"
+cp "$FONT_PATH" ~/Library/Fonts/
+rm "$FONT_PATH"
