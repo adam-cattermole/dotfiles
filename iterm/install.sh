32a9fff6e9a0948051fd55016cd98f0e176ba809
Adam Cattermole <a.d.cattermole@gmail.com>
2017-08-31 16:29:26 +0100

    Fix to correct iterm theme path

--- iterm/install.sh
+++ iterm/install.sh
@@ -3,7 +3,7 @@
 ###############################################################################
 
 # Install the DotGov theme for iTerm
-open "themes/DotGov.itermcolors"
+open "iterm/themes/DotGov.itermcolors"
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
