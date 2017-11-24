25b9755f170c47c672e8a70c188267d4384543fa
Adam Cattermole <a.d.cattermole@gmail.com>
2017-11-24 16:18:46 +0000

    Minor updates and new tmux.conf

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,7 +4,7 @@ set -e
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
   # Install Oh My Zsh if it isn't already present
-  if [[ ! -d $DOT/oh-my-zsh/ ]]; then
+  if [[ ! -d $HOME/.oh-my-zsh/ ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   fi
   # Set the default shell to zsh if it isn't currently set to zsh
