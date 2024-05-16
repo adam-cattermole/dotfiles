94a005475fc9e75b4824588a20d49692dd8393f0
Adam Cattermole <acatterm@redhat.com>
2024-05-16 15:29:36 +0100

    Update glog alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,7 +7,7 @@ then
 fi
 
 # The rest of my fun git aliases
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --decorate --abbrev-commit --date=relative"
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
