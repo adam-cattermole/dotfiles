4f5bb756e4c0823f8b619e868ed0fb53db4edb7e
Adam Cattermole <acatterm@redhat.com>
2024-05-16 16:49:29 +0100

    Update system

--- system/keys.zsh
+++
@@ -1,3 +0,0 @@
-# Pipe my public key to my clipboard.
-# Replaced by function in ../bin/pubkey
-# alias pubkey="more ~/.ssh/id_rsa.pub | xclip -sel clip | echo '=> Public key copied to pasteboard.'"
