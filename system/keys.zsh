586772402bcb6bf079896c1e4e308af095bb38fc
Adam Cattermole <acatterm@redhat.com>
2024-05-16 15:32:27 +0100

    Update bin

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,3 @@
 # Pipe my public key to my clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+# Replaced by function in ../bin/pubkey
+# alias pubkey="more ~/.ssh/id_rsa.pub | xclip -sel clip | echo '=> Public key copied to pasteboard.'"
