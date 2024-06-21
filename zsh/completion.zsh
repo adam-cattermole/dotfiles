567e618d946b635c6a41ce29183ce17ff8a5895c
Adam Cattermole <acatterm@redhat.com>
2024-06-21 22:12:55 +0100

    Dotfiles updates

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# kubectl completion
+source <(kubectl completion zsh)
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -16,6 +16,7 @@ setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
 setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
+# setopt EXTENDEDGLOB
 # setopt CORRECT
 setopt COMPLETE_IN_WORD
 
